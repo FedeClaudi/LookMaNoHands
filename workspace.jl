da3d04fc267f5ef3cacab70a00a73867c950dbba
FedeClaudi <federicoclaudi@protonmail.com>
2023-03-14 17:38:45 -0400

    opencv eye detection

--- workspace.jl
+++ workspace.jl
@@ -1,26 +1,8 @@
-import GLMakie, VideoIO
+using LookMaNoHands
+import LookMaNoHands: camera_warmup, snap
 
-cam = VideoIO.opencamera()
-try
-  img = read(cam)
+img = snap()
 
-  obs_img = GLMakie.Observable(GLMakie.rotr90(img))
-  scene = GLMakie.Scene(camera=GLMakie.campixel!, resolution=reverse(size(img)))
-  GLMakie.image!(scene, obs_img)
-  display(scene)
+using FileIO
 
-#   fps = VideoIO.framerate(cam)
-    fps = 30
-
-  @info "setup complete" size(img) fps
-
-  while GLMakie.isopen(scene)
-    img = read(cam)
-    obs_img[] = GLMakie.rotr90(img)
-    sleep(1/fps)
-  end
-# catch e
-#   @error e
-finally
-  close(cam)
-end
+save("test.png", img)
