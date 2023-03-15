974d03caea6d792b96bb3689883dd1e5c2021b23
FedeClaudi <federicoclaudi@protonmail.com>
2023-03-15 18:39:26 -0400

    cleanup

--- scripts/save_video.jl
+++ scripts/save_video.jl
@@ -4,11 +4,7 @@ import LookMaNoHands: save_video
 
 n_frames = 600
 
-<<<<<<< HEAD
-vidname = "vid2"
-=======
 vidname = "vid3"
->>>>>>> e30f29d74562d8d9cc725b3f88530214f66a4bd9
 dest = "/Users/federicoclaudi/Desktop/lmnh/videos"
 
 save_video(dest, vidname, n_frames)
