d53b23436f0caf42a566da2933407de06eebfd72
FedeClaudi <federicoclaudi@protonmail.com>
2023-03-19 09:31:10 -0400

    bump

--- test3.py
+++ test3.py
@@ -2,6 +2,7 @@ import mediapipe as mp
 import cv2
 import numpy as np
 import time
+from collections import namedtuple
 
 mp_face_mesh = mp.solutions.face_mesh
 
@@ -14,6 +15,11 @@ RIGHT_IRIS = [469, 470, 471, 472]
 LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]# Right eye indices list
 RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]
 
+_eye_corners = namedtuple('eye_corners', 'inner outer ')
+LEFT_CORNERS = _eye_corners(362, 263)
+RIGHT_CORNERS = _eye_corners(133, 33)
+
+N_LANDMARKS = 478
 
 def get_iris_position(iris_points, eye_points):
     # get the iris position "within" the eye as values between -1 and 1
@@ -71,13 +77,24 @@ with mp_face_mesh.FaceMesh(
         right = get_iris_position(right_iris, right_eye)
 
 
-        left_position, right_position = get_iris_position(left_iris, left_eye), get_iris_position(right_iris, right_eye)
-        print(f"Left position {left_position} - Right position {right_position}")
+        # left_position, right_position = get_iris_position(left_iris, left_eye), get_iris_position(right_iris, right_eye)
+        # print(f"Left position {left_position} - Right position {right_position}")
 
         # draw eyes and irises
         for color, points in zip(((0,0,255), (0,255,0)), ((left_iris, right_iris), (left_eye, right_eye))):
             for point in points:
                 cv2.polylines(frame, [point], True, color, 1, cv2.LINE_AA)
+
+        # draw every landmark
+        for i in range(N_LANDMARKS):
+            cv2.circle(frame, tuple(mesh_points[i]), 1, (255, 0, 0), -1)
+
+        # draw eye corners
+        for points in (LEFT_CORNERS, RIGHT_CORNERS):
+            cv2.circle(frame, tuple(mesh_points[points.inner]), 2, (0, 0, 255), -1)
+            cv2.circle(frame, tuple(mesh_points[points.outer]), 2, (0, 255, 0), -1)
+
+
 
         # Calculate the FPS and display it on the output image
         frame_count += 1
